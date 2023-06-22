5e2d5b1e89724cc8c9641d454c2ac551f0767094
Darián Capote <dariancapoteq@gmail.com>
2023-06-22 00:12:48 -0400

    Replace default offer images

--- src/helpers/defaults.ts
+++ src/helpers/defaults.ts
@@ -14,11 +14,11 @@ export const OFFERS: Array<Offer> = [
       { key: 'Estilo', value: 'Elegante' },
       { key: 'Durabilidad', value: 'Resistente de larga duración y fácil de cuidar' }
     ],
-    image: { src: 'sueter/1.png', alt: 'Sueter 1' },
+    image: { src: 'sueter/1.jpg', alt: 'Sueter 1' },
     gallery: [
-      { src: 'sueter/1.png', alt: 'Sueter 1' },
-      { src: 'sueter/2.png', alt: 'Sueter 2' },
-      { src: 'sueter/3.png', alt: 'Sueter 3' }
+      { src: 'sueter/1.jpg', alt: 'Sueter 1' },
+      { src: 'sueter/2.jpg', alt: 'Sueter 2' },
+      { src: 'sueter/3.jpg', alt: 'Sueter 3' }
     ],
     sell_price: 111.55,
     available: true,
@@ -36,11 +36,11 @@ export const OFFERS: Array<Offer> = [
       { key: 'Estilo', value: 'Elegante' },
       { key: 'Durabilidad', value: 'Resistente de larga duración y fácil de cuidar' }
     ],
-    image: { src: 'adidas/1.png', alt: 'Adidas 1' },
+    image: { src: 'adidas/1.jpg', alt: 'Adidas 1' },
     gallery: [
-      { src: 'adidas/1.png', alt: 'Adidas 1' },
-      { src: 'adidas/2.png', alt: 'Adidas 2' },
-      { src: 'adidas/3.png', alt: 'Adidas 3' }
+      { src: 'adidas/1.jpg', alt: 'Adidas 1' },
+      { src: 'adidas/2.jpg', alt: 'Adidas 2' },
+      { src: 'adidas/3.jpg', alt: 'Adidas 3' }
     ],
     sell_price: 105.2,
     available: true,
