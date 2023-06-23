592370c45e6e1220aa453b3707c36379547b5033
Adrian Capote Quintana <adriancapote95@gmail.com>
2023-06-23 09:08:48 -0400

    add new fields to api

--- @servimav/wings-services/services/shopOffer.ts
+++ @servimav/wings-services/services/shopOffer.ts
@@ -20,7 +20,9 @@ export default function init(api: AxiosInstance) {
     show: (id: number, params: { currency: string }) =>
       api.get<ShopOffer>(`${baseUrl}/${id}`, { params }),
     showClient: (id: number, params: { currency: string }) =>
-      api.get<ShopOffer>(`${baseUrl}/show-client/${id}`, { params })
+      api.get<ShopOffer>(`${baseUrl}/${id}/show-client`, { params }),
+    showSimilar: (id: number, params: { currency: string; sort?: SortFilterType }) =>
+      api.get<ShopOffer[]>(`${baseUrl}/${id}/similar`, { params })
   }
 }
 
@@ -52,9 +54,13 @@ export interface ShopOffer {
   // stock
   stock_type: STOCK_TYPE
   stock_qty: number
+  // Meta
+  rating: number
+  views: number
 }
 
-export interface ShopOfferCreate extends Omit<ShopOffer, 'id' | 'categories' | 'store'> {
+export interface ShopOfferCreate
+  extends Omit<ShopOffer, 'id' | 'categories' | 'store' | 'rating' | 'views'> {
   store_id: number
   categories?: number[]
 }
@@ -64,6 +70,8 @@ export interface ShopOfferFilter {
   store_id?: number
   currency?: CurrencyCode
   category_id?: number
+  sort?: SortFilterType
 }
 
 export type CurrencyCode = 'BTC' | 'CUP' | 'EUR' | 'MLC' | 'TRX' | 'USD' | 'USDT'
+export type SortFilterType = 'id' | 'price' | 'rating' | 'views'
