1fcd01ffd5858371a060683b3709c4ae1d5fc42a
Darián Capote <dariancapoteq@gmail.com>
2023-06-26 19:12:33 -0400

    Sample caret view with caret items

--- src/views/CaretView.vue
+++ src/views/CaretView.vue
@@ -1,7 +1,47 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+import { type Offer } from '@/types'
+import { STOCK_TYPE } from '@servimav/wings-services'
+
+/**
+ * -----------------------------------------
+ *	Components
+ * -----------------------------------------
+ */
+const CaretOfferWidget = defineAsyncComponent(
+  () => import('@/components/widgets/CaretOfferWidget.vue')
+)
+
+/**
+ * -----------------------------------------
+ *	Data
+ * -----------------------------------------
+ */
+const OFFERS: Array<Offer> = [
+  {
+    id: 1,
+    name: 'Tenis adidas',
+    image: '/offers/adidas/1.jpg',
+    available: true,
+    discount_price: 0,
+    production_price: 110,
+    sell_price: 120,
+    stock_type: STOCK_TYPE.LIMITED,
+    stock_qty: 10,
+    rating: 4.4,
+    views: 600
+  }
+]
+</script>
 
 <template>
-  <main>
-    <div class="text-xl text-center">Caret View</div>
+  <main class="p-2 pt-[4.8rem] pb-16 w-full container select-none">
+    <div class="flex flex-col items-center space-y-3 py-2">
+      <CaretOfferWidget :offer="OFFERS[0]" />
+      <CaretOfferWidget :offer="OFFERS[0]" />
+      <CaretOfferWidget :offer="OFFERS[0]" />
+      <CaretOfferWidget :offer="OFFERS[0]" />
+      <CaretOfferWidget :offer="OFFERS[0]" />
+    </div>
   </main>
 </template>
