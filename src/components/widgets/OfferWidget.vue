a38f77979cb77c35f257002d05be41eae2dd7ab6
Darián Capote <dariancapoteq@gmail.com>
2023-07-10 16:46:30 -0400

    Truncate name and fix image height

--- src/components/widgets/OfferWidget.vue
+++ src/components/widgets/OfferWidget.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { computed, defineAsyncComponent } from 'vue'
+import { computed } from 'vue'
 import { type Offer } from '@/types'
 import { toCurrency, setDefaultImage } from '@/helpers'
 /**
@@ -11,15 +11,7 @@ import { toCurrency, setDefaultImage } from '@/helpers'
 export interface Props {
   offer: Offer
 }
-/**
- * -----------------------------------------
- *	Components
- * -----------------------------------------
- */
 
-const ShoppingCartOutline = defineAsyncComponent(
-  () => import('@/components/icons/ShoppingCartOutline.vue')
-)
 /**
  * -----------------------------------------
  *	Composables
@@ -42,11 +34,11 @@ const displayPrice = computed(() =>
 
 <template>
   <div
-    class="relative w-full cursor-pointer rounded-lg border border-gray-100 bg-white p-1 transition-shadow hover:shadow"
+    class="relative w-full h-full cursor-pointer rounded-lg border border-gray-100 bg-white p-1 transition-shadow hover:shadow"
   >
-    <div class="mb-2 flex min-h-[12rem] items-center">
+    <div class="mb-2 flex items-center">
       <img
-        class="max-w-full rounded-lg"
+        class="w-full h-48 object-cover object-center rounded-lg"
         :src="offer.image ?? '/images/default.png'"
         :alt="offer.name"
         :title="offer.name"
@@ -54,14 +46,10 @@ const displayPrice = computed(() =>
       />
     </div>
     <div class="mt-2 px-1">
-      <div class="text-sm font-semibold tracking-wide text-gray-800">
+      <div class="text-sm font-medium text-gray-800 truncate">
         {{ offer.name }}
       </div>
       <div class="text-sm tracking-tight text-gray-600">{{ toCurrency(displayPrice) }}</div>
-    </div>
-
-    <div class="absolute right-3 top-3 cursor-pointer rounded-full bg-white p-1">
-      <ShoppingCartOutline class="h-3.5 w-3.5 text-gray-800" />
     </div>
   </div>
 </template>
