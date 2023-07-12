efe13ef649c9dccda10721ff49afca9ed51853f6
Adrian Capote Quintana <adriancapote95@gmail.com>
2023-07-12 08:27:48 -0400

    setup flowbite as plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	content: [
+		'./index.html',
+		'./src/**/*.{vue,js,ts,jsx,tsx}',
+		'./node_modules/flowbite/**/*.js'],
 	theme: {
 		extend: {
 			colors: {
@@ -43,5 +46,7 @@ module.exports = {
 			}
 		}
 	},
-	plugins: []
+	plugins: [
+		require('flowbite/plugin')
+	]
 }
