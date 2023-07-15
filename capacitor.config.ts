b368dc2aabe96e4bb39976f4d48af5960999dd61
Adrian Capote Quintana <adriancapote95@gmail.com>
2023-07-14 23:07:49 -0400

    debug build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,13 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.servimav.wings',
-  appName: 'Wings',
-  webDir: 'dist',
-  server: {
-    androidScheme: 'https'
-  }
+	appId: 'com.servimav.wings',
+	appName: 'Wings',
+	webDir: 'dist',
+	server: {
+		// androidScheme: 'https',
+		url: 'http://192.168.1.66:5173/'
+	}
 };
 
 export default config;
