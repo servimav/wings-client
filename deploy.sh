3df78ba8dcff2ed08866531a7b5cbd2d5236a30e
Adrian Capote Quintana <adriancapote95@gmail.com>
2023-07-16 16:09:58 -0400

    build script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,34 @@
-rm .env
+sshCommands="$(cat <<END_HEREDOC
+# Go To dir
+cd /var/www/servimav.com/wings/client/
+
+# Pull
+git pull
+
+# copy env
 cp .env.prod .env
-yarn build
-rm client-zip
-zip client.zip -r dist
-scp client.zip adricq@vps1.servimav.com:/var/www/servimav.com/wings/client/
+
+# Install api server dependencies.
+source ~/.nvm/nvm.sh
+
+echo "Switching to appropriate node.js version using nvm"
+if ! nvm use default ; then
+  echo "Error using nvm to switch node.js version."
+  exit 14
+fi
+
+# echo "Installing server api dependencies"
+# if ! yarn ; then
+#  echo "Error installing server api dependencies."
+  exit 15
+# fi
+
+echo "Installing building app"
+if ! yarn build ; then
+  echo "Error building app."
+  exit 16
+fi
+END_HEREDOC
+)"
+
+ssh adricq@vps1.servimav.com -t "$sshCommands"
