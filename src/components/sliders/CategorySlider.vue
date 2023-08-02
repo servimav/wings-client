f1d6d75acd5e05f6cc00ef1e42faeab752b04f1d
Darián Capote <dariancapoteq@gmail.com>
2023-08-01 22:15:21 -0400

    Not break line in label text

--- src/components/sliders/CategorySlider.vue
+++ src/components/sliders/CategorySlider.vue
@@ -48,7 +48,7 @@ function onCategoryClick(category: Category) {
     <div
       v-for="(category, index) in categories"
       :key="`category-slider-${index}`"
-      class="no-select cursor-pointer rounded-full px-3 py-2 text-center text-sm transition-colors"
+      class="no-select cursor-pointer whitespace-nowrap rounded-full px-3 py-2 text-center text-sm transition-colors"
       :class="[
         categoryActive && category.name == categoryActive.name
           ? 'bg-butterfly-blue-500 hover:bg-butterfly-blue-400 text-white'
